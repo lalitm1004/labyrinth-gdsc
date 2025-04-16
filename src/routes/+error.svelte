d1a5cd2f1f6806467006e8f4858e1fb13dfa0ede
lalitm1004 <lalitm1004@gmail.com>
2025-04-16 23:29:17 +0530

    fix: add 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<main class={`h-screen w-screen grid place-items-center text-white`}>
+    <div class={`flex flex-col justify-center items-center gap-2`}>
+        <p class={`text-3xl`}>404</p>
+        <a class={`border-2 rounded-lg p-2`} href={'/'}>Go Home</a>
+    </div>
+</main>
